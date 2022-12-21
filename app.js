4ff451e713485368d4fae8605392afd04f154f89
Yenifer Arrubla Bañol <yeniferarrubla@gmail.com>
2022-12-21 10:23:16 -0500

    add change views events

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
-/* Balance View */
+/* Header Features */
 
 /* Get elements */
 
 const $navbarBurger = $('#navBurger');
-const $navMenu = $('#navMenu')
+const $navMenu = $('#navMenu');
+const $balanceSec = $('#balance');
+const $categorySec = $('#categoriesSection');
+const $reportSec = $('#reportSection')
+
+/* Btns */
+const $btnBalance = $('#balanceLink');
+const $btnCategories = $('#categoriesLink');
+const $btnReports = $('#reportsLink');
 
 
 /* Functions */
@@ -15,7 +23,29 @@ const menuHamburger = () => {
     $navMenu.classList.toggle('is-active')
 }
 
+const openBalanceSec = () => {
+    $balanceSec.classList.remove('is-hidden')
+    $categorySec.classList.add('is-hidden')
+    $reportSec.classList.add('is-hidden')
+}
+
+const openCategorySec = () => {
+    $categorySec.classList.remove('is-hidden')
+    $balanceSec.classList.add('is-hidden')
+    $reportSec.classList.add('is-hidden')
+}
+
+const openReportSec = () => {
+    $reportSec.classList.remove('is-hidden')
+    $balanceSec.classList.add('is-hidden')
+    $categorySec.classList.add('is-hidden')
+}
+
 /* Events */
 $navbarBurger.addEventListener('click', menuHamburger);
 
+$btnCategories.addEventListener('click', openCategorySec);
 
+$btnReports.addEventListener('click', openReportSec);
+
+$btnBalance.addEventListener('click', openBalanceSec);
